086593aaf3b2e02b412e8d11387dfd70927d9365
duddk <duddk@scatterx.io>
2024-09-25 09:11:27 +0900

    할일 목록 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 // import Exercise from './Exercise'
 import CounterContainer from './containers/CounterComtainer'
+import TodosContainer from './containers/TodosContainer'
 
 function App() {
   return (
-    <div style={{padding: '1rem'}}>
-    {/* <Exercise/> */}
-      <CounterContainer />
-    </div>
+    <>
+      <div style={{padding: '1rem'}}>
+        {/* <Exercise /> */}
+        <CounterContainer />
+      </div>
+      <hr />
+      <div style={{padding: '1rem'}}>
+        <TodosContainer />
+      </div>
+      <hr />
+    </>
   )
 }
 
